baf18e3aa318c831676a0eb7c4bb278129b1197c
adamalbuchari <adamalbuchari1@gmail.com>
2025-06-10 14:28:16 +0800

    M=membuat method main

--- src/latihan2/DataDiri.java
+++ src/latihan2/DataDiri.java
@@ -2,10 +2,10 @@ package latihan2;
 
 public class DataDiri {
     String nama;
-    long npm;
+    String npm;
     String kelas;
 
-    public DataDiri(String nama, long npm, String kelas) {
+    public DataDiri(String nama, String npm, String kelas) {
         this.nama = nama;
         this.npm = npm;
         this.kelas = kelas;
---
+++ src/latihan3/RumusBeraksi.java
@@ -0,0 +1,20 @@
+package latihan3;
+
+// Nama  : Adam Al Buchari
+// NPM   : 2310010115
+// Kelas : 4A
+
+public class RumusBeraksi {
+    public static void main(String[] args) {
+
+        Rumus adam = new Rumus();
+
+        adam.setSisi(6);       
+        adam.setPanjang(8);    
+        adam.setLebar(5);       
+
+        adam.prosesLuasPersegi();
+
+        adam.prosesLuasPersegiPanjang();
+    }
+}
