5e323fb212eaec239f41151e6e289c3f3ce3732f
adamalbuchari <adamalbuchari1@gmail.com>
2025-06-10 13:47:45 +0800

    membuat objek

---
+++ src/latihan2/DataDiri.java
@@ -0,0 +1,19 @@
+package latihan2;
+
+public class DataDiri {
+    String nama;
+    long npm;
+    String kelas;
+
+    public DataDiri(String nama, long npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM  : " + npm);
+        System.out.println("Kelas: " + kelas);
+    }
+}
---
+++ src/latihan2/DataDiriBeraksi.java
@@ -0,0 +1,12 @@
+package latihan2;
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek dengan nama panggilan 'alex' dan mengisi data melalui konstruktor
+        DataDiri alex = new DataDiri("Alex Omega", "21000001", "IF-4C");
+
+        // Menampilkan data menggunakan method tampilkanData
+        alex.tampilkanData();
+    }
+
+}
